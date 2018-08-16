a0529d6334f761becca6f48d912f20ccacf584ed
施骁 <shxdev@163.com>
2018-08-16 23:58:42 +0800

    a

--- src/renderer.js
+++ src/renderer.js
@@ -18,40 +18,78 @@ HTMLElement.prototype.setRect=function(rect){
 
 document.addEventListener("DOMContentLoaded",(event)=>{
     const main_container=document.querySelector(".main-container");
-    main_container.filled_with_window=function(parent_window){
+    main_container.fn_filled_with_window=function(parent_window){
         const parent=parent_window||window;
         const style=window.getComputedStyle(this);
         this.setRect({
             x:0
             ,y:0
-            ,w:window.innerWidth-util.float(style["border-left-width"],0)-util.float(style["border-right-width"],0)
-            ,h:window.innerHeight-util.float(style["border-top-width"],0)-util.float(style["border-bottom-width"],0)
+            ,w:parent.innerWidth-util.float(style["border-left-width"],0)-util.float(style["border-right-width"],0)
+            ,h:parent.innerHeight-util.float(style["border-top-width"],0)-util.float(style["border-bottom-width"],0)
         });
     }
-    window.addEventListener("resize",(event)=>{
-        main_container.filled_with_window();
-    });
-    main_container.filled_with_window();
-    main_container.addEventListener("mousemove",(event)=>{
-        window.a=event;
-        const main_container=event.currentTarget;
-        const c_left=main_container.querySelector(".container.left");
-        const c_center=main_container.querySelector(".container.center");
-        const c_right=main_container.querySelector(".container.right");
-        const pointer={
-            x:event.clientX
-            ,y:event.clientY
-        };
-        const left_rect=c_left.getBoundingClientRect();
-        const right_rect=c_right.getBoundingClientRect();
-        const fuzzy=10;
-        if(
-            (pointer.x-left_rect.right>0 && pointer.x-left_rect.right<fuzzy) //鼠标在左侧栏边框
-            ||(right_rect.left-pointer.x>0 && right_rect.left-pointer.x<fuzzy) //鼠标在右侧栏边框
-        ){
-            main_container.style["cursor"]="col-resize";
-        }else{
-            main_container.style["cursor"]="unset";
+    main_container.fn_pointer_at_coloum_resize = function (pointer) {
+        const c_left = main_container.querySelector(".container.left");
+        const c_center = main_container.querySelector(".container.center");
+        const c_right = main_container.querySelector(".container.right");
+        const left_rect = c_left.getBoundingClientRect();
+        const right_rect = c_right.getBoundingClientRect();
+        const fuzzy = 10;
+        // if (Math.abs(pointer.x - left_rect.right)< fuzzy) {//鼠标在左侧栏边框
+        if (pointer.x - left_rect.right > 0 && pointer.x - left_rect.right < fuzzy) {//鼠标在左侧栏边框
+            return "left";
+        // } else if (Math.abs(pointer.x - right_rect.left) < fuzzy) { //鼠标在右侧栏边框
+        } else if (pointer.x-right_rect.left > 0 && pointer.x - right_rect.left < fuzzy){ //鼠标在右侧栏边框
+            return "right";
+        } else {
+            return false;
         }
+    };
+
+    window.addEventListener("resize",(event)=>{
+        main_container.fn_filled_with_window();
+    });
+    main_container.fn_filled_with_window();
+    main_container.addEventListener("pointermove",(event)=>{
+        const main_container=event.currentTarget;
+        const pointer = {
+            x: event.clientX
+            , y: event.clientY
+        };
+        if (main_container.isColumnResizing){
+            const { target, mouseDownPointer, mouseDownRect} = main_container.resizingData;
+            if (main_container.isColumnResizing==="left"){
+                target.style["width"] = `${mouseDownRect.width + (pointer.x - mouseDownPointer.x)}px`;
+            } else if (main_container.isColumnResizing === "right") {
+                target.style["width"] = `${mouseDownRect.width + (mouseDownPointer.x - pointer.x)}px`;
+            }
+
+        }else{
+            main_container.style["cursor"] = main_container.fn_pointer_at_coloum_resize(pointer) ? "col-resize" : "unset";
+
+        }
+    },true);
+    main_container.addEventListener("pointerdown", (event) => {
+        const main_container = event.currentTarget;
+        const pointer = {
+            x: event.clientX
+            , y: event.clientY
+        };
+        main_container.isColumnResizing = main_container.fn_pointer_at_coloum_resize(pointer);
+        main_container.setPointerCapture(event.pointerId);
+        if (main_container.isColumnResizing){
+            const target = (main_container.isColumnResizing === "left") ? main_container.querySelector(".container.left") : main_container.querySelector(".container.right") ;
+            main_container.resizingData={
+                target
+                , mouseDownPointer:pointer
+                , mouseDownRect:target.getBoundingClientRect()
+            }; 
+        }
+
+    },true);
+    main_container.addEventListener("pointerup", (event) => {
+        const main_container = event.currentTarget;
+        main_container.isColumnResizing=false;
+        main_container.releasePointerCapture(event.pointerId);
     });
 });
